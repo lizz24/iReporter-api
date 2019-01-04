df2fd5000d742deca41374284ccf161a24292641
lizz24 <39797444+lizz24@users.noreply.github.com>
2019-01-05 02:17:42 +0300

    Added get all redflag records

--- app/views/routes.py
+++ app/views/routes.py
@@ -35,3 +35,21 @@ def create_redflag():
         "message": "Created red-flag record"
         }]
     })
+
+@app.route('/api/v1/red-flags', methods=['GET'])
+def get_redflags():
+    # getting all red-flags
+    Redflags = [incident.get_incident() for incident in incident_list]
+    if len(incident_list) > 0:
+     return jsonify({
+         "data": Redflags,
+         "status" : 200
+        })
+    else:
+     return jsonify({
+             "error": "no data entry for red-flags",
+             "status": 404
+             })
+
+
+

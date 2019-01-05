eb067ac0afa45ced860885563da2759a23de53c4
lizz24 <39797444+lizz24@users.noreply.github.com>
2019-01-05 03:00:14 +0300

    Create incident_schema.py

---
+++ app/validators/incident_schema.py
@@ -0,0 +1,14 @@
+from marshmallow import Schema, fields,ValidationError
+from app.validators.validator import email, required
+
+class IncidentSchema(Schema):
+    #Represents the schema for incidents
+    type = fields.Str(required=False)
+    comment = fields.Str(required=True, validate=(required))
+    location = fields.Str(required=True, validate=(required))
+    id = fields.Int(required=False)
+    createdOn = fields.Str(required=False)
+    createdBy = fields.Int(required=False)
+    Images = fields.Str(required=False)
+    status = fields.Str(required=False)
+    Videos = fields.Str(required=False)

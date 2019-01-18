d1750bf143f5247ac87f8255e58be0539bf51126
lizz24 <39797444+lizz24@users.noreply.github.com>
2019-01-18 05:30:38 +0300

    added doc string commenting

--- app/validators/incident_schema.py
+++ app/validators/incident_schema.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields,ValidationError
 from app.validators.validator import email, required
 
 class IncidentSchema(Schema):
-    #Represents the schema for incidents
+    """Represents the schema for incidents"""
     type = fields.Str(required=False)
     comment = fields.Str(required=True, validate=(required))
     location = fields.Str(required=True, validate=(required))
